8452bba9fa642bd944ba1356f60c2203b4c7aa4e
QAway-to <aimindsetsad@gmail.com>
2026-06-17 12:31:05 +0400

    fix: correct lib/auth import path

--- pages/api/projects/cp.js
+++ pages/api/projects/cp.js
@@ -1,4 +1,4 @@
-const { requireAuth } = require('../../../../lib/auth')
+const { requireAuth } = require('../../../lib/auth')
 const { generateCp } = require('../../../lib/pythonClient')
 
 export default async function handler(req, res) {
