c2253b42b80f96523e2cf1d2a2bc49ff51a05d22
QAway-to <aimindsetsad@gmail.com>
2026-06-17 12:18:29 +0400

    security: require auth on workzilla/respond

--- pages/api/workzilla/respond.js
+++ pages/api/workzilla/respond.js
@@ -1,6 +1,9 @@
+const { requireAuth } = require('../../../../lib/auth')
 const { submitWorkzillaRespond } = require('../../../lib/pythonClient')
 
 export default async function handler(req, res) {
+  if (requireAuth(req, res)) return
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
