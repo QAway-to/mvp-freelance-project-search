1618ee7493f623f1c8c47475e877f3369d17f649
QAway-to <aimindsetsad@gmail.com>
2026-06-17 12:30:55 +0400

    fix: correct lib/auth import path

--- pages/api/projects/respond.js
+++ pages/api/projects/respond.js
@@ -1,4 +1,4 @@
-const { requireAuth } = require('../../../../lib/auth')
+const { requireAuth } = require('../../../lib/auth')
 const { submitRespond } = require('../../../lib/pythonClient')
 
 export default async function handler(req, res) {
